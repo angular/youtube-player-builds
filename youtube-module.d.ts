71e84bfd3fb985d988e6074ebff898b7bc82f931
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2022-05-03 18:16:00 +0000

    main - e31343f8f build: resolve type bundling issues (#24865)

--- youtube-module.d.ts
+++
@@ -1,7 +0,0 @@
-import * as i0 from "@angular/core";
-import * as i1 from "./youtube-player";
-export declare class YouTubePlayerModule {
-    static ɵfac: i0.ɵɵFactoryDeclaration<YouTubePlayerModule, never>;
-    static ɵmod: i0.ɵɵNgModuleDeclaration<YouTubePlayerModule, [typeof i1.YouTubePlayer], never, [typeof i1.YouTubePlayer]>;
-    static ɵinj: i0.ɵɵInjectorDeclaration<YouTubePlayerModule>;
-}
